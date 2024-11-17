a4ae8f4f542b71546e3fcdd6674332c1d70c376c
Wyn Van Devanter <wyn@opscanvas.com>
2024-11-16 20:14:04 -0500

    update registry ip

--- app/build-images.sh
+++ app/build-images.sh
@@ -4,14 +4,14 @@ TAG=v0.1
 cd producer
 sudo docker build -t producer . 
 sudo docker tag producer 192.168.5.169:5000/producer:$TAG
-sudo docker push  192.168.5.169:5000/producer
+sudo docker push  192.168.5.169:5000/producer:$TAG
 
 cd  ../consumer
 sudo docker build -t consumer . 
 sudo docker tag consumer 192.168.5.169:5000/consumer:$TAG
-sudo docker push  192.168.5.169:5000/consumer
+sudo docker push  192.168.5.169:5000/consumer:$TAG
 
 CD ../ml-server
 sudo docker build -t ml-server .
 sudo docker tag ml-server 192.168.5.169:5000/ml-server:$TAG
-sudo docker push  192.168.5.169:5000/ml-server
+sudo docker push  192.168.5.169:5000/ml-server:$TAG
