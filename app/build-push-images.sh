e83ac2f495323aedda4db444d9be9a0c908800dd
Chameleon Cloud User <wynvandev@gmail.com>
2024-12-08 17:54:02 +0000

    v0.3 for comsumer w increasd timeouts

--- app/build-push-images.sh
+++ app/build-push-images.sh
@@ -1,18 +1,18 @@
-TAG=v0.2
+TAG=v0.3
 
 echo "Building and pushing images to registry for tag: $TAG"
 
-cd producer
-sudo docker bq:quild -t producer . 
-sudo docker tag producer 192.168.5.169:5000/producer:$TAG
-sudo docker push  192.168.5.169:5000/producer:$TAG
+#cd producer
+#sudo docker bq:quild -t producer . 
+#sudo docker tag producer 192.168.5.169:5000/producer:$TAG
+#sudo docker push  192.168.5.169:5000/producer:$TAG
 
 cd  ../consumer
 sudo docker build -t consumer . 
 sudo docker tag consumer 192.168.5.169:5000/consumer:$TAG
 sudo docker push  192.168.5.169:5000/consumer:$TAG
 
-cd ../ml-server
-sudo docker build -t ml-server .
-sudo docker tag ml-server 192.168.5.169:5000/ml-server:$TAG
-sudo docker push  192.168.5.169:5000/ml-server:$TAG
+#cd ../ml-server
+#sudo docker build -t ml-server .
+#sudo docker tag ml-server 192.168.5.169:5000/ml-server:$TAG
+#sudo docker push  192.168.5.169:5000/ml-server:$TAG
